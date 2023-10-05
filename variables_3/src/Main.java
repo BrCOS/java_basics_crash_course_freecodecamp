fb61acaf130471143b1d4c2344408df095e41178
BrCOS <cosbrnta@gmail.com>
2023-10-05 14:59:03 -0300

    short (max and min values)

---
+++ variables_3/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        short max = Short.MAX_VALUE;
+        short min = Short.MIN_VALUE;
+
+        System.out.println("Max short value: " + max);
+        System.out.println("Min short value: " + min);
+    }
+}
