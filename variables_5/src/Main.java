2c22e0bb3d27836ce584c40d33368e3150c46507
BrCOS <cosbrnta@gmail.com>
2023-10-05 15:16:00 -0300

    char and boolean (data types)

---
+++ variables_5/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        char letter = 'A'; //store only one character
+        System.out.println(letter);
+
+        boolean isActive = false; //true or false only
+        System.out.println(isActive);
+    }
+}
