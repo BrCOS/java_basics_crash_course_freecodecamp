b80b11edb233be06a4ec5bec5461bcd0c1578576
BrCOS <cosbrnta@gmail.com>
2023-10-05 00:08:15 -0300

    primeiro commit

---
+++ variables/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void  main(String[] args) {
+        String name = "Bruno";
+        int age = 20;
+        System.out.println("Hello " + name + " Your age is " + age);
+    }
+}
