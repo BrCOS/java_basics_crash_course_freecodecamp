6f84b9f04e2f4ce0737eff9755cadbfb741bd373
BrCOS <cosbrnta@gmail.com>
2023-10-07 21:31:38 -0300

    if, else if and else

---
+++ if_statement_2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int year = 2020;
+
+        if (year == 2019) {
+            System.out.println("The year is " + year);
+        } else if (year == 2020) {
+            System.out.println("The year is " + year);
+        } else if (year == 2022) {
+            System.out.println("The year is " + year);
+        } else {
+            System.out.println("All conditions failed");
+        }
+
+        //we can add as much else if's as we want
+    }
+}
