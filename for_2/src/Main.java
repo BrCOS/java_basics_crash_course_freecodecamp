403ca38c487fb0f71f65b978faa59e88a39daf17
BrCOS <cosbrnta@gmail.com>
2023-10-16 22:13:53 -0300

    for loop, counting backards (100 to 0)

---
+++ for_2/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 0; i--) {//now we can do a decrementation (counting backwards)
+            System.out.println("i = " + i);
+        }
+    }
+}
