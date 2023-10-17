3200180f54226188ab14079e9e7f27e51100caf4
BrCOS <cosbrnta@gmail.com>
2023-10-16 22:12:13 -0300

    for loop, counting to ten

---
+++ for/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {//inside the parenthesis we declare a variable, then a condition and then a postfix incrementation
+            //it will execute the code 10 times, and when the incrementation hit the number 11 (in this case) the condition will be false and the loop will be ended
+            System.out.println("i = " + i);
+        }
+    }
+}
