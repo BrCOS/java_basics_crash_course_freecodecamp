6856b43d33974f67ccb28766f30c003ce8a01323
BrCOS <cosbrnta@gmail.com>
2023-10-18 21:55:04 -0300

    functions

---
+++ functions/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        sayHello("Bruno", 20);
+    }
+
+    public static void sayHello(String name, int age) {
+        System.out.println("Hello " + name + "!" + " Your age is " + age);
+    }
+}
