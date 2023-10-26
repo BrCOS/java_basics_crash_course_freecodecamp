9ed0dd5b7154d097585a0daa3b48b75274c11e45
BrCOS <cosbrnta@gmail.com>
2023-10-26 02:18:01 -0300

    OOP, classes, constructor, instances and template

---
+++ classes/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Tesla", "S PLAID", "Silver", 4);
+        Car car2 = new Car("Ford", "Mustang", "Red", 2);
+    }
+}
